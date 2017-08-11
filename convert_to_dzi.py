e5e9e7b3b9a58983471fca216bb23b8d308dd402
MasonMcGough <mcgough.mason@gmail.com>
2017-08-11 16:40:07 -0400

    Added convert_to_dzi.py file; Added new function to file_utils.py; Minor change to .gitignore;

---
+++ convert_to_dzi.py
@@ -0,0 +1,55 @@
+import deepzoom
+from os.path import basename, splitext, join
+from openslide import open_slide
+
+def get_level_with_max_pixels(src, max_pixels=50000000):
+    """
+    Return the largest level of Openslide-compatible image src with dimensions
+    smaller than max_pixels.
+
+    Inputs:
+        src - Path to image.
+        max_pixels - Maximum number of pixels in level. (default: 50000000)
+    Outputs:
+        opt_level - Largest level with fewer than max_pixels.
+    """
+
+    slide_dims = open_slide(SOURCE).level_dimensions
+
+    opt_level = -1
+    for i, dims in enumerate(slide_dims):
+        if reduce(lambda a, b: a*b, dims) < max_pixels:
+            opt_level = i
+            break
+
+    return opt_level
+
+def convert_to_dzi(src, dest='./', level=None, max_pixels=50000000, tile_size=254,
+                   tile_overlap=1, tile_format='jpg', image_quality=0.75,
+                   resize_filter='bicubic'):
+    """
+    Convert the image src to a dzi file and save in dest.
+    """
+
+    if level is None:
+        level = get_level_with_max_pixels(src, max_pixels)
+
+    slide_dims = open_slide(src).level_dimensions
+    slide = open_slide(src).read_region((0, 0), level, slide_dims[level])
+
+    # Create Deep Zoom Image creator with weird parameters
+    creator = deepzoom.ImageCreator(tile_size=tile_size,
+                                    tile_overlap=tile_overlap,
+                                    tile_format=tile_format,
+                                    image_quality=image_quality,
+                                    resize_filter=resize_filter)
+
+    # Create Deep Zoom image pyramid from source
+    name, _ = splitext(basename(src))
+    name_dzi = join(dest, name + '.dzi')
+    creator.create(slide, name_dzi)
+
+    return name_dzi
+
+if __name__ == '__main__':
+    print(convert_to_dzi('Consult 13-97.ndpi'))
--- file_utils.py
+++ file_utils.py
@@ -3,9 +3,35 @@ from os.path import join, isdir, isfile
 from re import sub
 from shutil import copy
 
+def list_files(loc, return_dirs=False, return_files=True):
+    """
+    Return a list of all filenames within a directory loc.
+
+    Inputs:
+        loc - Path to directory to list files from.
+    """
+
+    files = [join(loc, x) for x in listdir(loc)]
+
+    if return_dirs:
+        # check if file is directory and add it to output if so
+        is_dir = [isdir(x) for x in files]
+        new_dirs = [files[x] for x in range(len(files)) if is_dir[x]]
+    else:
+        new_dirs = []
+
+    if return_files:
+        # check if file is not directory and add it to output
+        is_file = [isfile(x) for x in files]
+        new_files = [files[x] for x in range(len(files)) if is_file[x]]
+    else:
+        new_files = []
+
+    return new_dirs + new_files
+
 def get_nested_dirs(locs=['.']):
     """
-    Return a list of all folders and subfolders within the directories in the list 
+    Return a list of all folders and subfolders within the directories in the list
     locs.
 
     Inputs:
@@ -17,16 +43,14 @@ def get_nested_dirs(locs=['.']):
 
     dirs = []
     for loc in locs:
-        # get files with full path
-        files = [join(loc, x) for x in listdir(loc)]
+        # get directories with full path
+        new_dirs = list_files(loc, return_files=False, return_dirs=True)
 
-        # check if file is directory and add it to output if so
-        is_dir = [isdir(x) for x in files]
-        new_dirs = [files[x] for x in range(len(files)) if is_dir[x]]
+        # traverse directories to the bottom
         nested_dirs = get_nested_dirs(new_dirs)
         dirs.extend(new_dirs)
         dirs.extend(nested_dirs)
-    
+
     return dirs
 
 def copy_files(loc, dest, rename=False):
@@ -37,7 +61,7 @@ def copy_files(loc, dest, rename=False):
         loc - Path to directory to copy files from.
         dest - Path to directory to copy files to.
         rename - Flag to rename files. If set to False, copied files use the same
-            basename. If True, the full path is used, with the slash characters 
+            basename. If True, the full path is used, with the slash characters
             replaced by hyphens.
 
     Outputs:
@@ -45,21 +69,18 @@ def copy_files(loc, dest, rename=False):
     """
 
     # get files with full path
-    files = [join(loc, x) for x in listdir(loc)]
+    files = list_files(loc)
 
-    # check if file is not directory and add it to output
-    is_file = [isfile(x) for x in files]
-    new_files = [files[x] for x in range(len(files)) if is_file[x]]
-    
     # copy files in list to dest
-    for this_file in new_files:
+    for this_file in files:
         # change name if renaming
         if rename:
             # replace slashes with hyphens to preserve unique name
             out_file = sub(r'\\|/', '-', this_file)
             out_file = join(dest, out_file)
             copy(this_file, out_file)
+            this_file = out_file
         else:
             copy(this_file, dest)
-    
-    return new_files
+
+    return files
