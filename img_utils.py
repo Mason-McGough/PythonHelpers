c8bc078f90b35717f156c6d3ec908f3abb1df649
JimBoonie <mcgough.mason@gmail.com>
2018-01-19 17:04:32 -0500

    Added include_excess input parameter to gridCrop; Began writing stitchCrops function;

--- img_utils.py
+++ img_utils.py
@@ -1,19 +1,29 @@
-def gridCrop(img, crop_dims, stride_size=None):
+from PIL import Image
+
+def gridCrop(img, crop_dims, stride_size=None, include_excess=True):
     """
     Split the image into tiles of smaller images.
+
+    Since the image dimensions may not be perfect multiples of the crop_dims, the 
+    edge portions of the image are included as crops positioned at img.size - 
+    crop_size. This results in the edge images overlapping slightly by default. This
+    behavior can be overridden by setting 'include_excess' to False.
 
     Inputs:
         img - PIL Image.
         crop_dims - The dimensions of each cropped image.
-        stride_size - The spacing between each cropped image, in pixels. (Default: 
+        stride_size - The offset between each cropped image, in pixels. (Default: 
                     crop_dims)
+        include_excess - When true, includes extra crops to include edges that would
+                         exceed the largest multiple of crop_dims within the image.
+                         (Default: True)
     Outputs:
-        crops - List of crop dicts containing img and other keys.
+        crop_imgs - List of crop dicts containing img and other keys.
     """
 
     img_dims = img.size # NOTE: Image.size is (width, height)
 
-    crop_corners = _gridCropCorners(img_dims, crop_dims, stride_size)
+    crop_corners = _gridCropCorners(img_dims, crop_dims, stride_size, include_excess)
 
     # loop through crop_corners and create crop for each
     crop_imgs = []
@@ -28,7 +38,7 @@ def gridCrop(img, crop_dims, stride_size=None):
 
     return crop_imgs
 
-def _gridCropCorners(im_dims, crop_size, stride_size=None):
+def _gridCropCorners(im_dims, crop_size, stride_size=None, include_excess=True):
     if stride_size is None:
         stride_size = crop_size
 
@@ -37,9 +47,10 @@ def _gridCropCorners(im_dims, crop_size, stride_size=None):
            and len(stride_size) == 2)
 
     c_indices = range(0, im_dims[0] - crop_size[0], stride_size[0])
-    c_indices.append(im_dims[0] - crop_size[0])
     r_indices = range(0, im_dims[1] - crop_size[1], stride_size[1])
-    r_indices.append(im_dims[1] - crop_size[1])
+    if include_excess:
+        c_indices.append(im_dims[0] - crop_size[0])
+        r_indices.append(im_dims[1] - crop_size[1])
 
     crop_corners = []
     crop_ctr = 0
@@ -48,3 +59,29 @@ def _gridCropCorners(im_dims, crop_size, stride_size=None):
             crop_corners.append((c, r))
 
     return crop_corners
+
+def stitchCrops(crop_imgs):
+    """
+    Merge a list of regularly-spaced cropped images into one single image.
+
+    Inputs:
+        crop_imgs - List of crop dicts containing the following keys:
+                        'img' - PIL image.
+                        'corner' - Tuple specifying the location of the upper-left
+                                   corner of the image.
+    Outputs:
+        img - Stitched image.
+    """
+
+    # determine mode of crops
+    mode = crop_imgs[0]['img'].mode
+
+    # check that all crops are of same mode
+    for crop in crop_imgs:
+        assert(crop['img'].mode == mode)
+
+    # find dimensions of original image
+    
+
+    # create output image 
+    img = Image.new(mode, size)
